fdff025df276e5f71f063234b6ab68d8e5ecf678
stevenktruong <steventruong@g.ucla.edu>
2018-06-21 15:50:31 -0700

    Organized scripts

--- js/math.js
+++ js/math.js
@@ -1,21 +1,4 @@
 $(document).ready(function() {
-  // Mobile menu button
-  let animated = false;
-  $('.menu-button').on('click', () => {
-    if (!animated) {
-      animated = true;
-      
-      // Toggle between X and three bars
-      $('.bottom-bar').toggleClass('rotate-bottom-bar');
-      $('.middle-bar').toggleClass('hide-middle-bar');
-      $('.top-bar').toggleClass('rotate-top-bar');
-
-      // Show menu
-      let newPos = ($('.nav-mobile-buttons').css('top') === '60px') ? '-120px' : '60px';
-      $('.nav-mobile-buttons').animate({ top: newPos }, 500, function() { animated = false });
-    }
-  });
-
   // Populate nav bar
   for (let i = 0; i < mathText.length; i++) {
     $('.math-nav').append(`
@@ -70,19 +53,4 @@ $(document).ready(function() {
       $('.math-content').animate({ opacity: 1 }, 250);
     });
   });
-
-  // Smooth scrolling
-  $('a[href*="#"]')
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $(`[name=${this.hash.slice(1)}]`);
-      if (target.length) {
-        event.preventDefault();
-        $('html, body').animate({ scrollTop: target.offset().top - 60 }, 500)
-      }
-    }
-  });
-})
+});
