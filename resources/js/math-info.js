025e43ba49902c4d194d67cf80fd71470367bf1a
stevenktruong <steventruong@g.ucla.edu>
2018-08-25 21:50:37 -0700

    Finally wrote some math and made each article its own page

---
+++ resources/js/math-info.js
@@ -0,0 +1,18 @@
+const homeText = `
+  <p>
+    This section is a work in progress.
+    $$e^{i \\pi} + 1 = 0.$$
+  </p>
+`;
+
+const mathInfo = [
+  {
+    title: "Machine Learning",
+    topics: [
+      {
+        title: "Neural Networks",
+        page: "neural-networks"
+      }
+    ]
+  },
+]
--- resources/js/math.js
+++ resources/js/math.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
   // Populate nav bar
-  for (let i = 0; i < mathText.length; i++) {
+  for (let i = 0; i < mathInfo.length; i++) {
     $('.math-nav').append(`
       <ul class="math-links">
         <li>
-          <a href="javascript:void(0)" class="expand-topics">${mathText[i].title}</a>
+          <a href="javascript:void(0)" class="expand-topics">${mathInfo[i].title}</a>
           <ul class="sub-links">` +
             (function() {
               let topicsString = "";
-              for (let j = 0; j < mathText[i].topics.length; j++) {
-                topicsString += `<li><a href="javascript:void(0)" class="change-topic" data-subject="${i}" data-topic="${j}">${mathText[i].topics[j].title}</a></li>`;
+              for (let j = 0; j < mathInfo[i].topics.length; j++) {
+                topicsString += `<li><a href="math/${mathInfo[i].topics[j].page}.html" class="change-topic" data-subject="${i}" data-topic="${j}">${mathInfo[i].topics[j].title}</a></li>`;
               }
               return topicsString;
             })() + `
@@ -26,31 +26,31 @@ $(document).ready(function() {
     toExpand.slideToggle(500);
   });
 
-  // Change a topic
-  $('.change-topic').on('click', function(event) {
-    let newSubject = '';
-    let newContent = '';
-    if ($(this).text() === 'Home') {
-      // If we click Home
-      newSubject = 'Home';
-      newContent = homeText;
-    } else {
-      // If we click anything else
-      let subjectIndex = $(this).attr('data-subject');
-      let topicIndex = $(this).attr('data-topic');
+  // // Change a topic
+  // $('.change-topic').on('click', function(event) {
+  //   let newSubject = '';
+  //   let newContent = '';
+  //   if ($(this).text() === 'Home') {
+  //     // If we click Home
+  //     newSubject = 'Home';
+  //     newContent = homeText;
+  //   } else {
+  //     // If we click anything else
+  //     let subjectIndex = $(this).attr('data-subject');
+  //     let topicIndex = $(this).attr('data-topic');
 
-      newSubject = mathText[subjectIndex].topics[topicIndex].title;
-      newContent = mathText[subjectIndex].topics[topicIndex].content;
-    }
+  //     newSubject = mathText[subjectIndex].topics[topicIndex].title;
+  //     newContent = mathText[subjectIndex].topics[topicIndex].content;
+  //   }
     
-    $('.math-content').animate({ opacity: 0 }, 250, function(event) {
-      $('.math-content').empty();
-      $('.math-content').append(`
-        <h3>${newSubject}</h3>
-        ${newContent}
-      `);
-      MathJax.Hub.Typeset();
-      $('.math-content').animate({ opacity: 1 }, 250);
-    });
-  });
+  //   $('.math-content').animate({ opacity: 0 }, 250, function(event) {
+  //     $('.math-content').empty();
+  //     $('.math-content').append(`
+  //       <h3>${newSubject}</h3>
+  //       ${newContent}
+  //     `);
+  //     MathJax.Hub.Typeset();
+  //     $('.math-content').animate({ opacity: 1 }, 250);
+  //   });
+  // });
 });
