deff228ecc3b93af20ec7b2dc96174365506f08b
stevenktruong <steventruong@g.ucla.edu>
2018-08-26 19:19:51 -0700

    Cleanup

--- resources/js/math.js
+++ resources/js/math.js
@@ -25,32 +25,4 @@ $(document).ready(function() {
     let toExpand = $(this).next();
     toExpand.slideToggle(500);
   });
-
-  // // Change a topic
-  // $('.change-topic').on('click', function(event) {
-  //   let newSubject = '';
-  //   let newContent = '';
-  //   if ($(this).text() === 'Home') {
-  //     // If we click Home
-  //     newSubject = 'Home';
-  //     newContent = homeText;
-  //   } else {
-  //     // If we click anything else
-  //     let subjectIndex = $(this).attr('data-subject');
-  //     let topicIndex = $(this).attr('data-topic');
-
-  //     newSubject = mathText[subjectIndex].topics[topicIndex].title;
-  //     newContent = mathText[subjectIndex].topics[topicIndex].content;
-  //   }
-    
-  //   $('.math-content').animate({ opacity: 0 }, 250, function(event) {
-  //     $('.math-content').empty();
-  //     $('.math-content').append(`
-  //       <h3>${newSubject}</h3>
-  //       ${newContent}
-  //     `);
-  //     MathJax.Hub.Typeset();
-  //     $('.math-content').animate({ opacity: 1 }, 250);
-  //   });
-  // });
 });
